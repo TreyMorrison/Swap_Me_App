ee1dfbd45b95980d4650948e58d49710a66d11e8
kishon hamilton <kishon.hamilton@bridgevine.com>
2018-01-20 18:43:08 -0500

    Created sign in scripts and page for app

---
+++ src/components/authenticate/init.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCZtIRh1OQ8q_ySc2QWOeIoXsearNv9nbg",
+    authDomain: "ganggreen-10c76.firebaseapp.com",
+    databaseURL: "https://ganggreen-10c76.firebaseio.com",
+    projectId: "ganggreen-10c76",
+    storageBucket: "ganggreen-10c76.appspot.com",
+    messagingSenderId: "574389336628"
+};
+
+firebase.initializeApp(config);
---
+++ src/components/authenticate/signIn.js
@@ -0,0 +1,8 @@
+var signIn = function(email,password){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+}
