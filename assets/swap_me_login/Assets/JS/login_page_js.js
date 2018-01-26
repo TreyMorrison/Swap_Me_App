1975cb725291bf25f6ac71199b1e6143e4c98cab
kishon hamilton <kishon.hamilton@bridgevine.com>
2018-01-26 04:56:18 -0500

    updated login page

---
+++ assets/swap_me_login/Assets/JS/login_page_js.js
@@ -0,0 +1,54 @@
+
+var signIn = function(email,password){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+              } else {
+                alert(errorMessage);
+              }
+              console.log(error);
+              
+        // ...
+    });
+}
+
+
+(function($){
+    $('#button-login').on('click', function(evt){
+        evt.preventDefault();
+        var email = $('#input-username').val();
+        var password = $('#input-password').val();
+
+        signIn(email, password);
+        window.location.replace(url='./profile.html');
+
+        
+    });
+
+    
+/*
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+
+          console.log('Display Name:', displayName, 'E-mail:', email, 'User ID:', uid);
+          
+          // ...
+        } else {
+          // User is signed out.
+          // ...
+        }
+      });*/
+})(jQuery);
